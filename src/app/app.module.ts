2f65089a69e1b4b678db6f32b17d8eaf77ef4485
Luckyfab <lukasz.faber@gmail.com>
2020-02-27 19:58:53 +0100

    2 files modified, 5 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { HomeComponent } from './home/home.component';
-
-import { Routes } from '@angular/router';
-const routes: Routes = [
- { path: '', component: HomeComponent },
- { path: 'home', component: HomeComponent }
-];
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { PokemonListComponent } from "./pokemon-list/pokemon-list.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LayoutModule } from "@angular/cdk/layout";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+//import { PokemonCardComponent } from "./pokemon-card/pokemon-card.component";
+import { HttpClientModule } from "@angular/common/http";
+import { MatPaginatorModule } from "@angular/material/paginator";
 
 @NgModule({
-  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule,RouterModule,
-                  MatSidenavModule,MatListModule ,
-                  MatToolbarModule, MatButtonModule ,
-                  MatIconModule, 
-                  RouterModule.forRoot(routes, {useHash: true}),
-                ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent ],
-  bootstrap:    [ AppComponent ],
-  providers: []
+  declarations: [AppComponent, PokemonListComponent, PokemonCardComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatCardModule,
+    HttpClientModule,
+    MatPaginatorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
